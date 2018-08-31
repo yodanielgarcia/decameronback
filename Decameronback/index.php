68cc3dac75a27b17135bddc2bab603523142b265
yodanielgarcia <31499858+yodanielgarcia@users.noreply.github.com>
2018-08-31 16:25:07 -0500

    Update index.php

--- Decameronback/index.php
+++ Decameronback/index.php
@@ -4,7 +4,7 @@
 header('Access-Control-Allow-Origin: *'); 
 // Create connection
 // Check connection
-$conn_string = "host='localhost' dbname=DecameronDB port=5432 user=postgres password=123456";
+$conn_string = "host='ec2-23-21-195-249.compute-1.amazonaws.com' dbname=d42t8kjbv0k8dh port=5432 user=eiwiwvlyakhcat password=7e3c59b0ef0e3e0dc0d7dfdaeb2d170c683f05dca8c6f729c0acec860c9dc7e7";
 $conn = pg_connect($conn_string) or die("Error conexion");
 pg_last_error();
 
